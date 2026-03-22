8c62441ca684dc3fd36837c9f036f1666a9c4710
Tahirmp3 <tahiramd2.0@gmail.com>
2026-03-22 21:39:01 +0000

    Adding fuctions files to github

---
+++ temprature_prediction.m
@@ -0,0 +1,16 @@
+function [outputArg1,outputArg2] = temprature_prediction(inputArg1,inputArg2)
+%TEMPRATURE_PREDICTION Summary of this function goes here
+%   Detailed explanation goes here
+arguments (Input)
+    inputArg1
+    inputArg2
+end
+
+arguments (Output)
+    outputArg1
+    outputArg2
+end
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
