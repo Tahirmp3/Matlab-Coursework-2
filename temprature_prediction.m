afa910be09a4f7cf1a550bdc3baa725e439f7d42
Tahirmp3 <tahiramd2.0@gmail.com>
2026-03-23 15:27:43 +0000

    Finished writing the code for task 3 and tested it  ran properly

--- temprature_prediction.m
+++ temprature_prediction.m
@@ -1,16 +1,72 @@
-function [outputArg1,outputArg2] = temprature_prediction(inputArg1,inputArg2)
-%TEMPRATURE_PREDICTION Summary of this function goes here
-%   Detailed explanation goes here
-arguments (Input)
-    inputArg1
-    inputArg2
+function temprature_prediction(a)
+% Documentation Block
+% Temprature_prediction predicts future temperature and provides LED warnings.
+% Temprature_prediction(a) continuously reads temperature data from a thermistor
+% It is connected to an Arduino, it converts the measured voltage to temperature,
+% and estimates the rate of change between consecutive readings. Using
+% this rate, the function predicts the temperature 5 minutes ahead. The
+% system then evaluates whether the temperature is rising or falling too
+% quickly and activates LEDs to indicate system status: green for stable
+% conditions, red for rapid increase, and yellow for rapid decrease. The
+% function operates in a continuous loop, providing real-time monitoring,
+% prediction, and visual feedback through both LEDs and command window output.
+
+%% CONSTANTS
+RATE_LIMIT = 4/60;      
+% Rate threshold (°C per second)
+
+%% INITIAL VALUES
+previoustemp = 0;           
+% Stores previous temprature
+
+time = 0;               
+% Counts time
+
+%% MAIN LOOP
+while true
+    
+    time = time + 1;    
+    % Increment time
+    
+    voltage = readVoltage(a,'A1');    
+    % Read voltage from the sensor
+    
+    temperature = (voltage - 0.500) / 0.010;    
+    % Converts voltage to temperature 
+    
+    rate = temperature - previoustemp;    
+    % Rate of change of temperature
+    
+    predictedtemp = temperature + rate * 300;    
+    % Predict temperature after 5 minutes (300 seconds)
+    
+    fprintf('Current Temp: %.2f °C\n', temperature);
+    fprintf('Predicted Temp (5 min): %.2f °C\n\n', predictedtemp);
+    % Outputs a display
+    
+    writeDigitalPin(a,'D2',0);
+    writeDigitalPin(a,'D3',0);
+    writeDigitalPin(a,'D4',0);
+    % Turn all LEDs OFF
+    
+    if rate > RATE_LIMIT
+        writeDigitalPin(a,'D4',1);
+        % Red LED if the temprature rises too fast
+        
+    elseif rate < -RATE_LIMIT
+        writeDigitalPin(a,'D3',1);
+        % Yellow LED if the temprature drops too fast
+        
+    else
+        writeDigitalPin(a,'D2',1);
+        % Green LED if the temprature is stable
+    end
+    
+    previoustemp = temperature;    
+    % Store current temperature
+    
+    pause(1);    
+    % Waits 1 second
 end
 
-arguments (Output)
-    outputArg1
-    outputArg2
-end
-
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
 end
