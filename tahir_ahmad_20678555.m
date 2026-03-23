3ae0b5a66d895cc4bbaf9a1d9143e74ee38f8473
Tahirmp3 <tahiramd2.0@gmail.com>
2026-03-23 00:51:09 +0000

    Completed task 1 recorded tempratures

--- tahir_ahmad_20678555.m
+++ tahir_ahmad_20678555.m
@@ -11,9 +11,75 @@ for blink = 1:15
 end
 % Loop for the LED to blink 15 times with 0.5 sec intervals
 %% TASK 1 - READ TEMPERATURE DATA, PLOT, AND WRITE TO A LOG FILE [20 MARKS]
+duration = 600;
+% Defined the duration 10 minutes/600 sec
+time = 0:1:duration;
+% Created a time array
+temprature = zeros(size(time));
+% For efficiency preallocated temprature array
+% Data acquisition loop
+for d_a_l = 1:length(time)
+    voltage = readVoltage(a,'A1');
+    % This would read voltage from sensor analog pin A1
+    temprature(d_a_l) = (voltage - 0.500)/0.010;
+    % This converts the voltage to temprature, equation from datasheet
+    pause(1);
+    % Pauses for 1 sec between readings
+end
 
-% Insert answers here
+% Statistical Analysis
+mintemp = min(temprature);
+% Minimum temprature that was recored
+maxtemp = max(temprature);
+% Maximum temprature that was recorded
+avgtemp = mean(temprature);
+% Average temprature that was recorded
 
+% Plotting
+figure
+plot(time,temprature)
+xlabel('time(s)')
+ylabel('temprature(°C)')
+title('temprature vs time graph')
+saveas(gcf,'temprature_vs_time_graph.png');
+% Saves the image of the graph
+
+% Output to cmd window
+fprintf('Data logging starts\n\n');
+% Header printed
+for d_a_l = 1:10
+    index = d_a_l * 60;
+    % This will calculate index for each minute
+    fprintf('minute\t\t%d\n', d_a_l);
+    fprintf('temperature\t%.2f C\n\n', temprature(index));
+end 
+% This will print data every minute
+
+% Summary Statistics
+fprintf('Maximum temp\t%.2f C\n', maxtemp);
+fprintf('Minimum temp\t%.2f C\n', mintemp);
+fprintf('Average temp\t%.2f C\n\n', avgtemp);
+fprintf('Data logging ends\n');
+
+% Writing data into a text file
+fileID = fopen('capsule_temperature.txt','w');
+% Opens file to write on
+fprintf(fileID,'Temperature Log\n\n');
+% Heading
+for d_a_l = 1:10
+    
+    index = d_a_l * 60;
+    
+    fprintf(fileID,'minute %d\n', d_a_l);
+    fprintf(fileID,'temperature %.2f C\n\n', temprature(index));
+end
+% This will write data by each minute
+fprintf(fileID,'Maximum temp %.2f C\n', maxtemp);
+fprintf(fileID,'Minimum temp %.2f C\n', mintemp);
+fprintf(fileID,'Average temp %.2f C\n', avgtemp);
+% This will write the statistics
+fclose(fileID);
+% Closes the file
 %% TASK 2 - LED TEMPERATURE MONITORING DEVICE IMPLEMENTATION [25 MARKS]
 
 % Insert answers here
